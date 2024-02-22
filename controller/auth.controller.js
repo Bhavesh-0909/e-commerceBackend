13ae88b682e7fffec73da795cb28b2f9e221e1bd
Bhavesh-0909 <bhaveshchoudhary0909@gmail.com>
2024-02-23 00:15:02 +0530

    signup controller code

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,5 +1,7 @@
 const pool = require('../database/pg.databse');
 const { ApiError } = require('../utils/ApiError.utils');
+const bcrypt = require('bcrypt');
+const { ApiResponse } = require('../utils/ApiResponse.utils');
 
 //signup user
 exports.signupUser = async (req, res) => {
@@ -13,8 +15,24 @@ exports.signupUser = async (req, res) => {
             throw new ApiError(400, "Password not matched");
         }
 
+        //validation
         const queryForUserExits = "SELECT email FROM users WHERE email = $1 AND account_type = $2";
-        const userExits = await pool.query()
+        const valuesForUserExits = [email, accountType]
+        const userExits = await pool.query(queryForUserExits, valuesForUserExits);
+        if(userExits){
+            throw new ApiError(400, "User already extis");
+        }
+
+        //insert in DB
+        password = bcrypt.hash(password, process.env.HASHING_ROUND);
+    
+        const query = "INSERT INTO users (user_name, user_address, account_type, email, user_password) VALUES ($1, $2, $3, $4, $5);";
+        const values = [name, address, accountType, email, password];
+        const createUser = await pool.query(query, values);
+
+        return res.status(200).json(
+            new ApiResponse(200, "User signup successfull")
+        )
 
         
     } catch (error) {
