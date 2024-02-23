d2bf639ffaed3469ad380af684c941b7beb2129b
Bhavesh-0909 <bhaveshchoudhary0909@gmail.com>
2024-02-23 20:18:51 +0530

    login controller code

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -3,7 +3,54 @@ const { ApiError } = require('../utils/ApiError.utils');
 const bcrypt = require('bcrypt');
 const { ApiResponse } = require('../utils/ApiResponse.utils');
 
-//signup user
+
+//************login user************
+exports.loginUser = async (req, res) =>{
+    try {
+        const {email, password} = req.body;
+        if(!email || !password){
+            throw new ApiError(400, "Fill all the details");
+        }
+
+        //DB call
+        const query = "SELECT * FROM users WHERE email = $1"
+        const values = [email];
+        const user = await pool.query(query, values);
+        console.log(user)
+        if(!user){
+            throw new ApiError(400, "User Does not exits");
+        }
+
+        if(await bcrypt.compare(password, user.user_password)){
+            const playload ={
+                id: user.user_id,
+                email: user.email,
+                accountType: user.account_type
+            }
+            const token = jwt.sign(playload, process.env.JWT_SECRET, {expiresIn:"2h",});
+
+            user.token = token;
+            user.password = undefined;
+
+            const options = {
+                expiresIn: new Date(Date.now() + 3*24*60*60*1000),
+                httpOnly:true,
+            }
+
+            return res.cookie("token", token, options).status(200).json(
+                new ApiResponse(200, "User Logined", user)
+            )
+
+        }else{
+            throw new ApiError(400, "Incorrect password")
+        }
+
+    } catch (error) {
+        throw new ApiError(500, error.message, error);
+    }
+}
+
+//************signup user***********
 exports.signupUser = async (req, res) => {
     try {
         const {name, address, email, password, confirmPassword, accountType} = req.body;
